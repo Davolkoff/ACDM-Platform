93ea1775370dc630b1f84f833411e628ec7abda6
Davolkoff <daniil.volkov.02@yandex.ru>
2022-06-20 12:42:19 +0700

    Fixed mintrewards task

--- tasks/stakingContract/mintRewards.ts
+++ tasks/stakingContract/mintRewards.ts
@@ -6,7 +6,7 @@ dotenv.config();
 task("mintrewards", "Mints 100000 XXX tokens to staking contract")
 .setAction(async (args, hre) => {
 
-    const rewardsToken = await hre.ethers.getContractAt("XXXToken", process.env.REWARDS_TOKEN_ADDRESS as string);
+    const rewardsToken = await hre.ethers.getContractAt("XXXToken", process.env.XXX_TOKEN_ADDRESS as string);
     
     await rewardsToken.mint(process.env.STAKING_CONTRACT_ADDRESS as string, "100000000000000000000000");
     console.log("Rewards tokens successfully minted");
